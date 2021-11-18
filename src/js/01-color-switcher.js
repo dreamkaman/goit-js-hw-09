43fbd6d14ca6cfd52f74f355a1640e4927854234
Vlad <64969216+dreamkaman@users.noreply.github.com>
2021-11-18 14:40:27 +0200

    next

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,7 +2,45 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 };
 
-const buttonsEl = document.querySelectorAll('button');
+const btnStartEl = document.querySelector('button[data-start]');
 
-console.log(buttonsEl['start']);
+const btnStopEl = document.querySelector('button[data-stop]');
 
+
+btnStopEl.disabled = true;
+
+let timerId;
+
+const setIntervalCallback = function () {
+  
+  document.body.style.backgroundColor = getRandomHexColor();
+
+};
+
+
+
+const onBtnClick = (event) => {
+  
+  if (event.target.hasAttribute('data-start')) {
+
+    btnStopEl.disabled = false;
+
+    btnStartEl.disabled = true;
+    
+    timerId = setInterval(setIntervalCallback, 1000);
+
+  };
+
+  if (event.target.hasAttribute('data-stop') && !btnStopEl.disabled) {
+
+    btnStartEl.disabled = false;
+    
+    clearInterval(timerId);
+
+    btnStopEl.disabled = true;
+
+  };
+
+};
+
+document.addEventListener('click', onBtnClick);
--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,1 +1,14 @@
 import flatpickr from "flatpickr";
+
+const inputEl = document.querySelector('input[id="datetime-picker"]');
+
+// const onInputClick = (event) => {
+//     flatpicker.toggle();
+    
+// };
+
+// inputEl.addEventListener('click', onInputClick)
+
+flatpickr(inputEl, {});
+
+console.log(inputEl);
